4fa33ceab6d35b354ca44531c1dfbe251de151ff
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2026-06-11 13:37:41 -0700

    first prototype

---
+++ src/event.rs
@@ -0,0 +1,86 @@
+use time::Date;
+use std::rc::{Rc, Weak};
+use std::ops::{Deref, DerefMut};
+use std::cmp::{PartialEq, Eq};
+use std::cell::RefCell;
+use std::sync::atomic::{AtomicU32, Ordering};
+
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Event(Rc<RefCell<_Event>>);
+
+impl Deref for Event {
+    type Target = Rc<RefCell<_Event>>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for Event {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl From<Rc<RefCell<_Event>>> for Event {
+    fn from(val: Rc<RefCell<_Event>>) -> Self {
+        Event(val)
+    }
+}
+
+impl PartialEq<Self> for _Event {
+    fn eq(&self, other: &Self) -> bool {
+        other.id == self.id
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        other.id != self.id
+    }
+}
+
+impl Eq for _Event {}
+
+#[derive(Debug)]
+pub struct _Event { // TODO: find a way to make this private but keep the other one public with
+                    // derefmut!
+    date: Date,
+    id: u32, 
+    desc: String,
+    parents: Vec<(Weak<RefCell<_Event>>, Vec<String>)>,
+    // children: Vec<Rc<RefCell<_Event>>>,
+    children: Vec<Event>
+}
+
+static NEXT_ID: AtomicU32 = AtomicU32::new(0);
+impl Event {
+    pub fn new(date: Date, desc: &str) -> Self {
+        let e = _Event {
+            date,
+            id: NEXT_ID.load(Ordering::Relaxed),
+            desc: desc.to_string(),
+            parents: vec![],
+            children: vec![],
+        };
+        NEXT_ID.fetch_add(1, Ordering::Relaxed);
+        Event(Rc::new(RefCell::new(e)))
+    }
+
+    pub fn add_child(&mut self, child: &mut Event, condition: Vec<String>) {
+        self.borrow_mut().children.push(child.clone());
+        child.borrow_mut().parents.push((Rc::<RefCell<_Event>>::downgrade(self), condition));
+    }
+
+    pub fn get_children(&self) -> Vec<Event> {
+        self.borrow().children.clone()
+    }
+
+    pub unsafe fn force_reset_id() {
+        NEXT_ID.store(0, Ordering::Relaxed);
+    }
+}
+
+impl ToString for Event {
+    fn to_string(&self) -> String {
+        format!("({})", self.borrow().id+1)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,44 @@
+mod event;
+
+use crate::event::Event;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    use time::macros::date;
+    use crate::Event;
+
+    #[test]
+    fn test_make_organizer() {
+        let mut root = Event::new(date!(2026-01-03), "Go to the store");
+        let mut eggs = Event::new(date!(2026-01-05), "make some eggs");
+        let mut sandwich = Event::new(date!(2026-01-07), "make a sandwich");
+        eggs.add_child(&mut sandwich, vec![]);
+        root.add_child(&mut eggs, vec![]);
+
+        assert_eq!(root.get_children(), vec![eggs]);
+        assert_eq!(root.get_children()[0].get_children(), vec![sandwich]);
+    }
+
+    #[test]
+    fn test_print_event() {
+        unsafe { Event::force_reset_id(); } // need to reset the ids because we made a bunch up
+                                            // there.
+        let root = Event::new(date!(2026-01-03), "Go to the store");
+        assert_eq!(root.to_string(), "(1)".to_string());
+    }
+
+    #[test]
+    fn test_dependent_events() {
+        let mut root = Event::new(date!(2026-01-03), "Go to the store");
+        let mut eggs = Event::new(date!(2026-01-05), "make some eggs");
+        let mut sandwich = Event::new(date!(2026-01-05), "make a sandwich");
+        root.add_child(&mut eggs, vec!["bought eggs".to_string()]);
+        root.add_child(&mut sandwich, vec!["bought bread".to_string(), "bought ham".to_string()]);
+
+        assert_eq!(root.get_children(), vec![eggs, sandwich]);
+    }
+}
